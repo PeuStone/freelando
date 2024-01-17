6a5fe9935e6727b3a444dac125a3f34337d09857
PeuStone <pedrohmotaaa@gmail.com>
2024-01-17 10:40:43 -0300

    Criação e estilização do componente Campo Texto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Cabecalho } from "./componentes/Cabecalho";
+import { CampoTexto } from "./componentes/CampoTexto";
 import { Card } from "./componentes/Card";
 import { Estilos } from "./componentes/EstilosGlobais";
 import { ProvedorTema } from "./componentes/ProvedorTema";
@@ -16,6 +17,7 @@ function App() {
         <Tipografia variante="body" componente="body">
           Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
         </Tipografia>
+        <CampoTexto titulo="Nome Completo" />
       </Card>
     </ProvedorTema>
   );
