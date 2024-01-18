7a7c8e57511aeff315809a944372aca5a366944a
PeuStone <pedrohmotaaa@gmail.com>
2024-01-18 11:26:04 -0300

    Ajustes dos componentes para utilização do Grind System

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,30 +1,6 @@
 import styled from "@emotion/styled"
-import logo from './assets/Logo.png'
 
-const HeaderEstilizado = styled.header`
+export const Cabecalho = styled.header`
     background: ${props => props.theme.cores.primarias.a};
     padding: ${props => props.theme.espacamentos.m};
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
 `
-
-const BotaoEstilizado = styled.button`
-    border: none;
-    background: ${props => props.theme.cores.primarias.a};
-    color: ${props => props.theme.cores.neutras.c};
-    font-size: 20px;
-`
-
-export const Cabecalho = () => {
-    return (
-        <HeaderEstilizado>
-            <div>
-                <img src={logo} alt="Logo da Freelando" />
-            </div>
-            <div>
-                <BotaoEstilizado>Login</BotaoEstilizado>
-            </div>
-        </HeaderEstilizado>
-    )
-}
--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -1,13 +1,7 @@
 import styled from "@emotion/styled"
 
-const RodapeEstilizado = styled.footer`
+export const Rodape = styled.footer`
     background: ${props => props.theme.cores.primarias.a};
     padding: ${props => props.theme.espacamentos.xl};
     color: ${props => props.theme.cores.branco};
 `
-
-export const Rodape = () => {
-    return (
-        <RodapeEstilizado />
-    )
-}
