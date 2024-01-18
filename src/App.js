883dcdd4035bcfc3e0340281a66ddee5d7fbafbf
PeuStone <pedrohmotaaa@gmail.com>
2024-01-18 09:44:15 -0300

    Utilizando do sistema de Grid para posicionar o Card

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Col, Container, Row } from "react-grid-system";
 import { Cabecalho } from "./componentes/Cabecalho";
 import { CampoTexto } from "./componentes/CampoTexto";
 import { Card } from "./componentes/Card";
@@ -11,15 +12,21 @@ function App() {
     <ProvedorTema>
       <Estilos />
       <Cabecalho />
-      <Card>
-        <Tipografia variante="h1" componente="h1">
-          Freelando
-        </Tipografia>
-        <Tipografia variante="body" componente="body">
-          Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
-        </Tipografia>
-        <CampoTexto titulo="Nome Completo" />
-      </Card>
+      <Container>
+        <Row justify="center">
+          <Col lg={6}>
+            <Card>
+              <Tipografia variante="h1" componente="h1">
+                Freelando
+              </Tipografia>
+              <Tipografia variante="body" componente="body">
+                Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
+              </Tipografia>
+              <CampoTexto titulo="Nome Completo" />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
       <Rodape />
     </ProvedorTema>
   );
