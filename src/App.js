25b43802a7615fce09cb10ccdb4698ec94df4af7
PeuStone <pedrohmotaaa@gmail.com>
2024-01-19 09:22:48 -0300

    Criação do componente da lista suspensa

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import { IconeTwitter } from "./componentes/Icones/IconeTwitter";
 import { Link } from "./componentes/Link";
 import { ListaInline } from './componentes/ListaInline'
 import { ItemListaInline } from './componentes/ListaInline/ItemListaInline'
+import { ListaSuspensa } from "./componentes/ListaSuspensa";
 
 function App() {
   return (
@@ -49,7 +50,7 @@ function App() {
               </Row>
               <Row>
                 <Col lg={4} md={4} sm={4}>
-                  <CampoTexto titulo="Estado" />
+                  <ListaSuspensa titulo="Estado" />
                 </Col>
                 <Col lg={8} md={8} sm={8}>
                   <CampoTexto titulo="Cidade" />
