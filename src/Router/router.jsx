fcaaac8d2608282fa5ccc3ecba6da823b68c7798
PeuStone <pedrohmotaaa@gmail.com>
2024-01-23 09:47:59 -0300

    Refatoração das rotas com implementação de uma rota aninhada

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,10 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import PaginaInicial from "../paginas/PaginaInicial";
+import SelecaoCliente from "../paginas/cadastro/SelecaoCliente";
+import LayoutBaseCadastro from "../paginas/cadastro/LayoutBaseCadastro";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <PaginaInicial />,
-        children: [],
+        children: [
+            {
+                path: 'cadastro',
+                element: <LayoutBaseCadastro />,
+                children: [
+                    {
+                        path: '',
+                        element: <SelecaoCliente />,
+                    },
+                    {
+                        path: 'cliente',
+                        element: <h1>Interesses</h1>
+                    },
+                    {
+                        path: 'dados-pessoais',
+                        element: <h1>Dados pessoais</h1>
+                    },
+                    {
+                        path: 'concluido',
+                        element: <h1>Concluído</h1>
+                    }
+                ]
+            }
+        ],
     },
 ]);
