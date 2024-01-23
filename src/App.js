fa51f984b2fc73123cc58046b0620eec02073a5c
PeuStone <pedrohmotaaa@gmail.com>
2024-01-23 09:46:41 -0300

    Ajustes para todas as paginas receberem os Temas e Estilos Globais

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./Router/router"
+import { ProvedorTema } from "./componentes/ProvedorTema"
+import { Estilos } from "./componentes/EstilosGlobais"
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <ProvedorTema>
+      <Estilos />
+      <RouterProvider router={router} />
+    </ProvedorTema>
+  )
 }
 
 export default App
