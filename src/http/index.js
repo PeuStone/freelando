70c40fe02327f0b58eb0349e2b9d18bb47a0b31b
PeuStone <pedrohmotaaa@gmail.com>
2024-02-03 10:28:42 -0300

    Implementando interceptador para tratar o refresh do token do usuário logado

--- src/http/index.js
+++ src/http/index.js
@@ -6,7 +6,7 @@ const http = axios.create({
 })
 
 http.interceptors.request.use(function (config) {
-    const token = ArmazenadorToken.AccessToken
+    const token = ArmazenadorToken.accessToken
     if (token) {
         config.headers.Authorization = `Bearer ${token}`
         console.log('o token foi adicionado ao cabeçalho');
@@ -15,5 +15,40 @@ http.interceptors.request.use(function (config) {
 }, function (error) {
     Promise.reject(error);
 });
+
+const rotasIgnoradasPelosErros = [
+    'auth/login',
+    'auth/refresh',
+]
+
+const tentaRenovarToken = async () => {
+    const token = ArmazenadorToken.refreshToken
+    return axios.get('http://localhost:8080/auth/refresh', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(resposta => {
+        ArmazenadorToken.definirTokens(
+            resposta.data.access_token,
+            resposta.data.refresh_token
+        )
+    })
+}
+
+const lidarComErro401 = async (erro) => {
+    await tentaRenovarToken()
+        .then(() => http(erro.config))
+    return Promise.reject(erro)
+}
+
+http.interceptors.response.use(
+    (response) => response,
+    function (error) {
+        if (!rotasIgnoradasPelosErros.includes(error.config.url) && error.response.status === 401) {
+            return lidarComErro401(error)
+        }
+
+        return Promise.reject(error);
+    });
 
 export default http
